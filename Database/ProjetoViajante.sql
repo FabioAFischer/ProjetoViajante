579d09a4e17b97acfa497b6f7d7edb712bdae8ed
pachecobeatriz <beatriz.santos13@alunos.sc.senac.br>
2025-06-02 01:36:42 -0300

    backup - june 2

--- Database/ProjetoViajante.sql
+++ Database/ProjetoViajante.sql
@@ -7,69 +7,104 @@ USE ProjetoViajante;
 
 CREATE TABLE Usuario (
   idUser INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nomeUser VARCHAR(255),
-  emailUser VARCHAR(255),
-  senhaUser VARCHAR(255),
-  dataCadastroUser DATE,
-  dataExclusaoUser DATE
+  
+  user_nome VARCHAR(100) NOT NULL,
+  user_email VARCHAR(100) NOT NULL,
+  user_senha VARCHAR(16) NOT NULL
   );
-
-
-CREATE TABLE Viagem (
+  
+  
+  CREATE TABLE Viagem (
   idViagem INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  tituloViagem VARCHAR(255),
-  idUser INT, FOREIGN KEY (idUser) REFERENCES Usuario (idUser)
+  
+  viagem_titulo VARCHAR(100) NOT NULL,
+  viagem_dataPartida DATE,
+  viagem_dataChegada DATE,
+  
+  idUser INT, FOREIGN KEY (idUser) REFERENCES Usuario (idUser) ON DELETE CASCADE ON UPDATE CASCADE
+  );
+  
+  
+  CREATE TABLE Lugar (
+	idLocal INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    
+    lugar_cep INT,
+    lugar_rua VARCHAR(100),
+    lugar_bairro VARCHAR(100),
+    lugar_numero INT,
+    lugar_complemento VARCHAR(100),
+    lugar_cidade VARCHAR(100),
+    lugar_estado VARCHAR(100),
+    lugar_pais VARCHAR(100),
+    
+    idViagem INT, FOREIGN KEY (idViagem) REFERENCES Viagem (idViagem) ON DELETE CASCADE ON UPDATE CASCADE,
+    idUser INT, FOREIGN KEY (idUser) REFERENCES Viagem (idUser) ON DELETE CASCADE ON UPDATE CASCADE
   );
 
 
 CREATE TABLE Mochila (
   idMochila INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  tituloMochila VARCHAR(255),
-  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Viagem (idViagem),
-  idUser INT, FOREIGN KEY (idUser) REFERENCES Viagem (idUser)
+  
+  mochila_titulo VARCHAR(100) NOT NULL,
+   mochila_pesoTotalAprox INT,
+  
+  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Viagem (idViagem) ON DELETE CASCADE ON UPDATE CASCADE,
+  idUser INT, FOREIGN KEY (idUser) REFERENCES Viagem (idUser) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 CREATE TABLE MochilaItem (
   idMochilaItem INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nomeMochilaItem VARCHAR(255),
-  quantMochilaItem VARCHAR(255),
-  idMochila INT, FOREIGN KEY (idMochila) REFERENCES Mochila (idMochila),
-  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Mochila (idViagem),
-  idUser INT, FOREIGN KEY (idUser) REFERENCES Mochila (idUser)
+  
+  mochilaItem_nome VARCHAR(100),
+  mochilaItem_quant INT NOT NULL,
+  mochilaItem_descricao VARCHAR(500),
+  
+  idMochila INT, FOREIGN KEY (idMochila) REFERENCES Mochila (idMochila) ON DELETE CASCADE ON UPDATE CASCADE,
+  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Mochila (idViagem) ON DELETE CASCADE ON UPDATE CASCADE,
+  idUser INT, FOREIGN KEY (idUser) REFERENCES Mochila (idUser) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 CREATE TABLE Despesa (
   idDespesa INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  tituloDespesa VARCHAR(255),
-  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Viagem (idViagem),
-  idUser INT, FOREIGN KEY (idUser) REFERENCES Viagem (idUser)
+  
+  despesa_titulo VARCHAR(100) NOT NULL,
+  despesa_valorTotal INT,
+  
+  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Viagem (idViagem) ON DELETE CASCADE ON UPDATE CASCADE,
+  idUser INT, FOREIGN KEY (idUser) REFERENCES Viagem (idUser) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 
 CREATE TABLE DespesaItem (
   idDespesaItem INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  nomeDespesaItem VARCHAR(255),
-  precoDespesaItem INT,
-  idDespesa INT, FOREIGN KEY (idDespesa) REFERENCES Despesa (idDespesa),
-  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Despesa (idViagem),
-  idUser INT, FOREIGN KEY (idUser) REFERENCES Despesa (idUser)
+  
+  despesaItem_nome VARCHAR(100) NOT NULL,
+  despesaItem_valor INT,
+  despesaItem_descricao VARCHAR(500),
+  
+  idDespesa INT, FOREIGN KEY (idDespesa) REFERENCES Despesa (idDespesa) ON DELETE CASCADE ON UPDATE CASCADE,
+  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Despesa (idViagem) ON DELETE CASCADE ON UPDATE CASCADE,
+  idUser INT, FOREIGN KEY (idUser) REFERENCES Despesa (idUser) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
-
-CREATE TABLE Agenda (
-  idAgenda INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  dataPartidaAgenda DATE,
-  dataChegadaAgenda DATE,
-  idViagem INT, FOREIGN KEY (idViagem) REFERENCES Viagem (idViagem),
-  idUser INT, FOREIGN KEY (idUser) REFERENCES Viagem (idUser)
-);
-
+/*
 select * from Usuario;
 select * from Viagem;
+select * from Lugar;
 select * from Mochila;
 select * from MochilaItem;
 select * from Despesa;
 select * from DespesaItem;
-select * from Agenda;
+*/
+
+/*
+  insert into Usuario (user_nome, user_email, user_senha) values ('teste', 'teste@gmail.com', 'ssssssss');
+  select * from Usuario;
+  
+  insert into Viagem (viagem_titulo, viagem_dataPartida, viagem_dataChegada, idUser) values ('viajinha', '2025-11-08', '2025-12-25', 1);
+  select * from Viagem;
+  
+  update Usuario set idUser = 2 where idUser = 1;
+  /*
